390d89578d7e7d383075ce9a4613757a8c717b38
Synchx <hatta.asnan00@gmail.com>
2022-09-13 14:24:39 +0700

    Menyelesaikan template pada local direktori

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_catalog(request):
+    data_barang_catalog = CatalogItem.objects.all()
+    context = {
+    'list_barang': data_barang_catalog,
+    'nama': 'Son Sulung Suryahatta Asnan',
+    'id' : '2106751455'
+    
+}
+    return render(request, "katalog.html", context)
