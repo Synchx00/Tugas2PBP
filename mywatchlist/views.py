285812c1180759b7e769b78d0d932939a1fb2c70
Synchx <hatta.asnan00@gmail.com>
2022-09-22 09:16:38 +0700

    Menambahkan poin bonus

--- mywatchlist/views.py
+++ mywatchlist/views.py
@@ -6,11 +6,20 @@ from django.core import serializers
 # Create your views here.
 def show_film_list(request):
     data_watchlist = WatchListItem.objects.all()
+
+    watched = WatchListItem.objects.filter(watched="Watched")
+    not_watched = WatchListItem.objects.filter(watched="Not watched")
+
+    watched_count = watched.count()
+    not_watched_count = not_watched.count()
+    
+    watched_status = watched_count >= not_watched_count
+
     context = {
     'list_film': data_watchlist,
     'nama': 'Son Sulung Suryahatta Asnan',
-    'id' : '2106751455'
-    
+    'id' : '2106751455',
+    'status_check' : watched_status,
 }
     return render(request, "mywatchlist.html", context)
 
